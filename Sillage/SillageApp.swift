dbf0d495aa22e2faa2b09fbd71e3258d2bb49e31
alcyone.diy <alcyone.diy@gmail.com>
2026-03-19 23:43:25 +0100

    Empty app

---
+++ Sillage/SillageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillageApp.swift
+//  Sillage
+//
+//  Created by Alcyone on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SillageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
