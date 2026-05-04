fae223e4df6c455ec1d491796afd5e3e7a2cce71
alcyone-diy <249823388+alcyone-diy@users.noreply.github.com>
2026-05-04 21:47:20 +0000

    feat: Add COG vector preferences
    Added persistent data layer support for the COG (Course Over Ground) predictive vector. Updated `PreferencesService` to conform to `@Observable` and `@MainActor` for Swift 6 safety. Refactored all configuration properties to stored properties to ensure SwiftUI reactivity. The service now securely wraps a raw time interval double into a `Measurement<UnitDuration>` property while keeping `UserDefaults` sync encapsulated within its `didSet` observers.

--- Sillage/Helpers/PreferencesService.swift
+++ Sillage/Helpers/PreferencesService.swift
@@ -10,7 +10,9 @@
 
 import Foundation
 import CoreLocation
+import Observation
 
+@MainActor
 protocol PreferencesServiceProtocol {
   var savedMapSource: String? { get set }
   var savedGeoGarageLayerID: String? { get set }
@@ -22,68 +24,106 @@ protocol PreferencesServiceProtocol {
   var hasAcceptedDisclaimer: Bool { get set }
   var isOpenSeaMapOverlayEnabled: Bool { get set }
 
+  var isCOGVectorEnabled: Bool { get set }
+  var cogVectorTimeHorizon: Measurement<UnitDuration> { get set }
+  var isCOGVectorTicksEnabled: Bool { get set }
+
   func saveCameraState(coordinate: CLLocationCoordinate2D, zoom: Double, direction: Double)
   func loadCameraState() -> (coordinate: CLLocationCoordinate2D, zoom: Double, direction: Double)?
 }
 
+@Observable
+@MainActor
 class PreferencesService: PreferencesServiceProtocol {
-  static let shared = PreferencesService()
+  @ObservationIgnored static let shared = PreferencesService()
 
-  private let mapSourceKey = "selectedMapSource"
-  private let savedGeoGarageLayerIDKey = "savedGeoGarageLayerID"
-  private let savedLatitudeKey = "savedLatitude"
-  private let savedLongitudeKey = "savedLongitude"
-  private let savedZoomKey = "savedZoom"
-  private let savedDirectionKey = "savedDirection"
-  private let gloveModeEnabledKey = "gloveModeEnabled"
-  private let hasAcceptedDisclaimerKey = "hasAcceptedDisclaimer"
-  private let isOpenSeaMapOverlayEnabledKey = "isOpenSeaMapOverlayEnabled"
+  @ObservationIgnored private let mapSourceKey = "selectedMapSource"
+  @ObservationIgnored private let savedGeoGarageLayerIDKey = "savedGeoGarageLayerID"
+  @ObservationIgnored private let savedLatitudeKey = "savedLatitude"
+  @ObservationIgnored private let savedLongitudeKey = "savedLongitude"
+  @ObservationIgnored private let savedZoomKey = "savedZoom"
+  @ObservationIgnored private let savedDirectionKey = "savedDirection"
+  @ObservationIgnored private let gloveModeEnabledKey = "gloveModeEnabled"
+  @ObservationIgnored private let hasAcceptedDisclaimerKey = "hasAcceptedDisclaimer"
+  @ObservationIgnored private let isOpenSeaMapOverlayEnabledKey = "isOpenSeaMapOverlayEnabled"
 
-  private let defaults = UserDefaults.standard
+  @ObservationIgnored private let isCOGVectorEnabledKey = "isCOGVectorEnabled"
+  @ObservationIgnored private let cogVectorTimeHorizonSecondsKey = "cogVectorTimeHorizonSeconds"
+  @ObservationIgnored private let isCOGVectorTicksEnabledKey = "isCOGVectorTicksEnabled"
+
+  @ObservationIgnored private let defaults = UserDefaults.standard
 
   var savedMapSource: String? {
-    get { defaults.string(forKey: mapSourceKey) }
-    set { defaults.set(newValue, forKey: mapSourceKey) }
+    didSet { defaults.set(savedMapSource, forKey: mapSourceKey) }
   }
 
   var savedGeoGarageLayerID: String? {
-    get { defaults.string(forKey: savedGeoGarageLayerIDKey) }
-    set { defaults.set(newValue, forKey: savedGeoGarageLayerIDKey) }
+    didSet { defaults.set(savedGeoGarageLayerID, forKey: savedGeoGarageLayerIDKey) }
   }
 
   var savedLatitude: Double? {
-    get { defaults.object(forKey: savedLatitudeKey) as? Double }
-    set { defaults.set(newValue, forKey: savedLatitudeKey) }
+    didSet { defaults.set(savedLatitude, forKey: savedLatitudeKey) }
   }
 
   var savedLongitude: Double? {
-    get { defaults.object(forKey: savedLongitudeKey) as? Double }
-    set { defaults.set(newValue, forKey: savedLongitudeKey) }
+    didSet { defaults.set(savedLongitude, forKey: savedLongitudeKey) }
   }
 
   var savedZoom: Double? {
-    get { defaults.object(forKey: savedZoomKey) as? Double }
-    set { defaults.set(newValue, forKey: savedZoomKey) }
+    didSet { defaults.set(savedZoom, forKey: savedZoomKey) }
   }
 
   var savedDirection: Double? {
-    get { defaults.object(forKey: savedDirectionKey) as? Double }
-    set { defaults.set(newValue, forKey: savedDirectionKey) }
+    didSet { defaults.set(savedDirection, forKey: savedDirectionKey) }
   }
 
   var gloveModeEnabled: Bool {
-    get { defaults.bool(forKey: gloveModeEnabledKey) }
-    set { defaults.set(newValue, forKey: gloveModeEnabledKey) }
+    didSet { defaults.set(gloveModeEnabled, forKey: gloveModeEnabledKey) }
   }
 
   var hasAcceptedDisclaimer: Bool {
-    get { defaults.bool(forKey: hasAcceptedDisclaimerKey) }
-    set { defaults.set(newValue, forKey: hasAcceptedDisclaimerKey) }
+    didSet { defaults.set(hasAcceptedDisclaimer, forKey: hasAcceptedDisclaimerKey) }
   }
 
   var isOpenSeaMapOverlayEnabled: Bool {
-    get { defaults.bool(forKey: isOpenSeaMapOverlayEnabledKey) }
-    set { defaults.set(newValue, forKey: isOpenSeaMapOverlayEnabledKey) }
+    didSet { defaults.set(isOpenSeaMapOverlayEnabled, forKey: isOpenSeaMapOverlayEnabledKey) }
+  }
+
+  var isCOGVectorEnabled: Bool {
+    didSet { defaults.set(isCOGVectorEnabled, forKey: isCOGVectorEnabledKey) }
+  }
+
+  var isCOGVectorTicksEnabled: Bool {
+    didSet { defaults.set(isCOGVectorTicksEnabled, forKey: isCOGVectorTicksEnabledKey) }
+  }
+
+  private var rawCogVectorTimeHorizonSeconds: Double {
+    didSet { defaults.set(rawCogVectorTimeHorizonSeconds, forKey: cogVectorTimeHorizonSecondsKey) }
+  }
+
+  var cogVectorTimeHorizon: Measurement<UnitDuration> {
+    get {
+      Measurement(value: rawCogVectorTimeHorizonSeconds, unit: .seconds)
+    }
+    set {
+      rawCogVectorTimeHorizonSeconds = newValue.converted(to: .seconds).value
+    }
+  }
+
+  init() {
+    self.savedMapSource = defaults.string(forKey: mapSourceKey)
+    self.savedGeoGarageLayerID = defaults.string(forKey: savedGeoGarageLayerIDKey)
+    self.savedLatitude = defaults.object(forKey: savedLatitudeKey) as? Double
+    self.savedLongitude = defaults.object(forKey: savedLongitudeKey) as? Double
+    self.savedZoom = defaults.object(forKey: savedZoomKey) as? Double
+    self.savedDirection = defaults.object(forKey: savedDirectionKey) as? Double
+    self.gloveModeEnabled = defaults.bool(forKey: gloveModeEnabledKey)
+    self.hasAcceptedDisclaimer = defaults.bool(forKey: hasAcceptedDisclaimerKey)
+    self.isOpenSeaMapOverlayEnabled = defaults.bool(forKey: isOpenSeaMapOverlayEnabledKey)
+
+    self.isCOGVectorEnabled = defaults.object(forKey: isCOGVectorEnabledKey) as? Bool ?? true
+    self.isCOGVectorTicksEnabled = defaults.object(forKey: isCOGVectorTicksEnabledKey) as? Bool ?? true
+    self.rawCogVectorTimeHorizonSeconds = defaults.object(forKey: cogVectorTimeHorizonSecondsKey) as? Double ?? 3600.0
   }
 
   func saveCameraState(coordinate: CLLocationCoordinate2D, zoom: Double, direction: Double) {
