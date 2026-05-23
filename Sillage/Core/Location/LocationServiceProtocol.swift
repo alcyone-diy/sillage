c2da51e38b115f50aad604431d54a445efb17e5b
alcyone.diy <alcyone.diy@gmail.com>
2026-05-23 13:41:32 +0200

    Move LocationServiceProtocol in Sillage/Core/Location

---
+++ Sillage/Core/Location/LocationServiceProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  LocationServiceProtocol.swift
+//  Alcyone Sillage
+//
+//  Created by Alcyone on 2026-05-23.
+//  Copyright © 2026 Alcyone.
+//  This file is released under the MIT License.
+//  See LICENSE file in the project root for full license information.
+//
+
+import Foundation
+import CoreLocation
+
+protocol LocationServiceProtocol {
+  var locationUpdates: AsyncStream<CLLocation> { get }
+  var authorizationStatusStream: AsyncStream<CLAuthorizationStatus> { get }
+
+  func requestAuthorization()
+  func startUpdatingLocation()
+  func stopUpdatingLocation()
+  
+  func requestBackgroundLocation() -> any BackgroundLocationToken
+}
