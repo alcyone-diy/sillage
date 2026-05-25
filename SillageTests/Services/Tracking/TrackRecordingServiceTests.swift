ffb066a6b680d695f9e99ef60f2ef836e92a5c81
alcyone.diy <alcyone.diy@gmail.com>
2026-05-25 17:26:47 +0200

    Update license for TrackRecordingServiceTests.swift

--- SillageTests/Services/Tracking/TrackRecordingServiceTests.swift
+++ SillageTests/Services/Tracking/TrackRecordingServiceTests.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Alcyone on 2026-05-24.
 //  Copyright © 2026 Alcyone.
+//  This file is released under the MIT License.
+//  See LICENSE file in the project root for full license information.
 //
 
 import Testing
