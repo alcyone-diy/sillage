275c7765f9afab24b719c20696bf83fb5955e61e
alcyone.diy <alcyone.diy@gmail.com>
2026-07-01 01:56:25 +0200

    update contextual menu colors

--- Sillage/Features/Chart/Views/MapLibreView.swift
+++ Sillage/Features/Chart/Views/MapLibreView.swift
@@ -18,12 +18,14 @@ struct PopoverMenuAction: Identifiable {
   let title: String
   let systemImage: String
   let isDestructive: Bool
+  let isCancelAction: Bool
   let action: () -> Void
   
-  init(title: String, systemImage: String, isDestructive: Bool = false, action: @escaping () -> Void) {
+  init(title: String, systemImage: String, isDestructive: Bool = false, isCancelAction: Bool = false, action: @escaping () -> Void) {
     self.title = title
     self.systemImage = systemImage
     self.isDestructive = isDestructive
+    self.isCancelAction = isCancelAction
     self.action = action
   }
 }
@@ -37,10 +39,10 @@ struct PopoverMenuView: View {
         Button(action: action.action) {
           HStack {
             Text(action.title)
-              .foregroundColor(action.isDestructive ? .red : .primary)
+              .foregroundColor(action.isDestructive ? .red : (action.isCancelAction ? MarineTheme.Colors.cancelAction : .primary))
             Spacer()
             Image(systemName: action.systemImage)
-              .foregroundColor(action.isDestructive ? .red : .primary)
+              .foregroundColor(action.isDestructive ? .red : (action.isCancelAction ? MarineTheme.Colors.cancelAction : .primary))
           }
           .padding()
           .frame(height: 50)
@@ -459,7 +461,7 @@ struct MapLibreView: UIViewRepresentable {
         let actionTitle = isSelected ? String(localized: "Deselect") : String(localized: "Select")
         let actionImageName = isSelected ? MarineIcon.deselect.rawValue : MarineIcon.select.rawValue
         
-        let selectAction = PopoverMenuAction(title: actionTitle, systemImage: actionImageName) { [weak self] in
+        let selectAction = PopoverMenuAction(title: actionTitle, systemImage: actionImageName, isCancelAction: isSelected) { [weak self] in
           guard let self = self else { return }
           Task { @MainActor in
             if isSelected {
@@ -501,7 +503,7 @@ struct MapLibreView: UIViewRepresentable {
         actions.append(createAction)
         
         if self.parent.viewModel.goToWaypointID != nil {
-          let deselectAction = PopoverMenuAction(title: String(localized: "Deselect Target"), systemImage: MarineIcon.deselect.rawValue, isDestructive: true) { [weak self] in
+          let deselectAction = PopoverMenuAction(title: String(localized: "Deselect Target"), systemImage: MarineIcon.deselect.rawValue, isCancelAction: true) { [weak self] in
             Task { @MainActor in
               self?.parent.waypointService?.setDestination(waypointID: nil)
             }
