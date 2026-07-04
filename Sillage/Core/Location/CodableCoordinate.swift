95b6ef76b405a1700303cc19f60b68968abec933
alcyone.diy <alcyone.diy@gmail.com>
2026-07-04 03:13:21 +0200

    Add AnchorWatch model

---
+++ Sillage/Core/Location/CodableCoordinate.swift
@@ -0,0 +1,32 @@
+//
+//  CodableCoordinate.swift
+//  Alcyone Sillage
+//
+//  Created by Alcyone on 2026-07-04.
+//  Copyright © 2026 Alcyone.
+//  This file is released under the MIT License.
+//  See LICENSE file in the project root for full license information.
+//
+
+import Foundation
+import CoreLocation
+
+/// Structure intermédiaire pour gérer la sérialisation de CLLocationCoordinate2D
+public struct CodableCoordinate: Codable, Sendable, Equatable {
+  public let latitude: CLLocationDegrees
+  public let longitude: CLLocationDegrees
+  
+  public init(_ coordinate: CLLocationCoordinate2D) {
+    self.latitude = coordinate.latitude
+    self.longitude = coordinate.longitude
+  }
+  
+  public init(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+  
+  public var coordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+  }
+}
